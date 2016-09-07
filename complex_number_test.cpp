09d1afc34198c10a3f773b433e0cbae2bb66c156
Ted Mader <ted.mader@ge.com>
2016-09-07 09:45:57 -0500

    Update test function for addition

--- complex_number_test.cpp
+++ complex_number_test.cpp
@@ -2,15 +2,12 @@
 #include "complex_number.hpp"
 using namespace std;
 
-int main(int argc, char *argv[]) {
-
-}
-
 void testAdd() {
   ComplexNumber cn1(1, 2);
   ComplexNumber cn2(3, 4);
   cn1 = cn1 + cn2;
-  cout << &cn1 << endl;
+  cout << cn1.getA() << endl;
+  cout << cn1.getB() << endl;
 }
 
 void testSubtract() {
@@ -23,4 +20,8 @@ void testMultiply() {
 
 void testDivide() {
   cout << "SUCCESS" << endl;
+}
+
+int main(int argc, char *argv[]) {
+  testAdd();
 }
