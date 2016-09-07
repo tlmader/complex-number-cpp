cf5621d171ae7b3937ff886fb06a44e3b368a1ff
Ted Mader <ted.mader@ge.com>
2016-09-07 09:38:35 -0500

    Implement constructor and copy methods

--- complex_number.cpp
+++ complex_number.cpp
@@ -2,6 +2,21 @@
 #include "complex_number.hpp"
 using namespace std;
 
+ComplexNumber::ComplexNumber() {
+  a = 0;
+  b = 0;
+}
+
+ComplexNumber::ComplexNumber(float _a, float _b) {
+  a = _a;
+  b = _b;
+}
+
+ComplexNumber::ComplexNumber(const ComplexNumber &rhs) {
+  a = rhs.a;
+  b = rhs.b;
+}
+
 ComplexNumber ComplexNumber::operator+(const ComplexNumber& other) {
   ComplexNumber cn;
   cn.a = this->a + other.getA();
